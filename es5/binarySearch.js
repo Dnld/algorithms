580b79eab9572c14b60067ec5b092c822b389542
Dnld <steinert.donald@gmail.com>
2015-12-01 08:44:21 -0500

    implements insertion sort in Swift

--- es5/binarySearch.js
+++ es5/binarySearch.js
@@ -1,4 +1,4 @@
-var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
 var binarySearch = function(input, key, inputMax, inputMin) {
   inputMax = inputMax || input.length;
---
+++ swift/insertionSort.swift
@@ -0,0 +1,19 @@
+var arr = [12,2,1,6,15,13,8,14,3,9,16,5,10,4,7,11]
+
+func insertionSort(var input: [Int]) -> [Int]{
+    var item, i: Int
+    
+    outer: for x in 0..<input.count {
+        item = input[x]
+        for i = x; i >= 0; i-- {
+            if item < input[i] {
+                input.removeAtIndex(i + 1)
+                input.insert(item, atIndex: i)
+            }
+        }
+    }
+    
+    return input
+}
+
+print(insertionSort(arr))
